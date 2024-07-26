d5c35a25ea5a5ab13c11b5717798a8a59bb17707
rajkumar@123 <160397769+rajkumar-chauhan@users.noreply.github.com>
2024-07-26 08:08:40 +0530

    Update AnsibleCD.groovy

--- src/org/foo/AnsibleCD.groovy
+++ src/org/foo/AnsibleCD.groovy
@@ -1,1 +1,22 @@
+package org.foo
+
+class AnsibleCD implements Serializable {
+    def script
+
+    AnsibleCD(script) {
+        this.script = script
+    }
+
+    def checkoutPlaybookRepo(String repoUrl, String credentialsId) {
+        script.git url: repoUrl, credentialsId: credentialsId
+    }
+
+    def runAnsiblePlaybook(String playbookPath, String inventoryFile) {
+        script.sh "ansible-playbook -i ${inventoryFile} ${playbookPath}"
+    }
+
+    def notifySlack(String channel, String message, String color) {
+        script.slackSend(channel: channel, message: message, color: color)
+    }
+}
 
